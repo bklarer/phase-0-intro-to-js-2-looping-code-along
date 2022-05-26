cde8928accd79ae7e408d91ac990ab87a6d6ce71
Benjamin Klarer <benjaminklarer@gmail.com>
2022-05-25 17:34:51 -0700

    commit tree 94090dc3bb51648ddbcc97fe3ad1b908ea89d9c3

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(name, msg = "surprise") {
+    let cards = []
+    for (let i = 0; i < name.length; i++) {
+    cards.push(`Thank you, ${name[i]}, for the wonderful ${msg} gift!`)
+    }
+    return cards   
+}
+
+function countDown(n) {
+    let count = n
+    while(count >= 0) {
+    console.log(count)
+    count -= 1
+}}
